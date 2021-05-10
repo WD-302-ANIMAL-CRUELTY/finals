9ff0b5c64e848768104f2afce5c717894c219acb
Timothy Caballa <tfcaballa@gmail.com>
2021-05-10 10:13:20 +0800

    May 10 commit
    Added animations.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 import {  Title } from "@angular/platform-browser";
+import { bounceAnimation,
+  flashAnimation,
+  pulseAnimation,
+  rubberBandAnimation,
+  bounceInUpOnEnterAnimation,
+  bounceInAnimation,
+  bounceInDownAnimation,
+  bounceInLeftAnimation,
+  bounceInRightAnimation,
+  bounceInUpAnimation
+} from 'angular-animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    bounceInUpOnEnterAnimation({ anchor: 'enter1' }),
+    bounceInUpOnEnterAnimation({ anchor: 'enter2', delay: 100 }),
+    bounceInUpOnEnterAnimation({ anchor: 'enter3', delay: 200 }),
+    bounceInUpOnEnterAnimation({ anchor: 'enter4', delay: 300 }),
+    bounceInUpOnEnterAnimation({ anchor: 'enter5', delay: 400 }),
+  ]
 })
 export class AppComponent {
   title = 'finals';
@@ -13,3 +31,4 @@ export class AppComponent {
   }
 
 }
+
--- src/app/petitions/petitions.component.ts
+++ src/app/petitions/petitions.component.ts
@@ -1,9 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  bounceAnimation,
+  flashAnimation,
+  pulseAnimation,
+  rubberBandAnimation,
+  shakeAnimation,
+  swingAnimation,
+  rotateAnimation
+} from 'angular-animations';
 @Component({
   selector: 'app-petitions',
   templateUrl: './petitions.component.html',
-  styleUrls: ['./petitions.component.css']
+  styleUrls: ['./petitions.component.css'],
+  animations: [
+    pulseAnimation({ anchor: 'pulse' }),
+    rotateAnimation({ anchor: 'rotate90', degrees: 90 }),
+  ]
 })
 export class PetitionsComponent implements OnInit {
 
